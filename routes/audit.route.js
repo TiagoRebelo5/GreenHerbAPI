fc81e30b6d5d9399d6a31f3f2f8c0a173cbb7a16
TiagoRebelo5 <tmartinsrebelo@gmail.com>
2026-05-14 15:39:55 +0100

    idk

--- routes/audit.route.js
+++ routes/audit.route.js
@@ -6,6 +6,7 @@ router.get('/', controller.getAll);
 router.post('/', controller.create);
 router.get('/:id', controller.getById);
 router.put('/:id', controller.update);
+
 router.delete('/:id', controller.delete);
 
 module.exports = router;
